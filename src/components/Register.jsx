af3439943bf2866e246004bf320a3036e2d543e0
kaushal-345 <kaushal.kulkarni345@gmail.com>
2025-03-27 10:59:46 +0530

    added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,35 +1,52 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./Register.css";
 
 export default function Register() {
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
+  const [msg, setMsg] = useState();
+  const msgRef=useRef();
 
   const handleSubmit = () => {
-    const exists=users.some(
-      (u) => u.name===user.name && u.email===user.email && u.password===user.password
-    );
-      if(exists){
-        alert("user exists")
-      }else{
-        setUsers([...users, user]);
-      }
-  };//also try with array ka find (apparently) array.find,filter,map 
+    const found = users.find((value) => value.email === user.email);
+    if (found) {
+      setMsg("User already exists");
+      msgRef.current.style.color = "red";
+    } else {
+      setMsg();
+      setUsers([...users, user]);
+      setUser({ ...user, name: "", email: "", password: "" });
+    }
+  };
 
-  const handleDelete = (task) => {
-    setUsers(users.filter((value) => value!==task));
+  // const handleSubmit = () => {
+  //   const exists=users.some(
+  //     (u) => u.name===user.name && u.email===user.email && u.password===user.password
+  //   );
+  //     if(exists){
+  //       alert("user exists")
+  //     }else{
+  //       setUsers([...users, user]);
+  //     }
+  // };
+  //also try with array ka find (apparently) array.find,filter,map 
+
+  const handleDelete = (email) => {
+    setUsers(users.filter((value) => value!==email));
   }
 
   return (
     <div>
-      <h3>Registration Form</h3>
       <div className="App-Row">
         <div className="App-Box">
+        <h3>Registration Form</h3>
+        <p ref={msgRef}>{msg}</p>
           <p>
             <input 
             type="text" 
+            value={user.name}
             placeholder='Enter Name'
             onChange={(e)=>setUser({...user, name: e.target.value})}
             ></input>
@@ -37,6 +54,7 @@ export default function Register() {
           <p>
             <input 
             type="text" 
+            value={user.email}
             placeholder='Email address'
             onChange={(e)=>setUser({...user, email: e.target.value})}
             ></input>
@@ -44,6 +62,7 @@ export default function Register() {
           <p>
             <input 
             type="password" 
+            value={user.password}
             placeholder='New password'
             onChange={(e)=>setUser({...user, password: e.target.value})}
             ></input>
